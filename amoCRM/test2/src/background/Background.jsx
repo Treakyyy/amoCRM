5b9b10c32681c19d4af8cb90ec983f0c5abf31f3
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-05-06 03:59:25 +0500

    Create Background.jsx

---
+++ amoCRM/test2/src/background/Background.jsx
@@ -0,0 +1,15 @@
+import './Background.css'
+
+const Background = () => {
+    return(
+        <div>
+            <div className='purpleLight'></div>
+            <div className='redLight'></div>
+            <div className='purpleBall'></div>
+            <div className='redBigBall'></div>
+            <div className='redSmallBall'></div>
+        </div>
+    )
+}
+
+export default Background
